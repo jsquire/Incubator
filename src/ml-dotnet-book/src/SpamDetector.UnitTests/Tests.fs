3285d55738bd21f00265421d00f0f213d7c66466
Jesse Squire <jesse.squire@gmail.com>
2018-02-10 18:49:51 -0500

    Initial scaffolding for chapter 2 sample

---
+++ src/ml-dotnet-book/src/SpamDetector.UnitTests/Tests.fs
@@ -0,0 +1,8 @@
+module MachineLearningBook.SpamDetector
+
+open System
+open Xunit
+
+[<Fact>]
+let ``My test`` () =
+    Assert.True(true)
---
+++ src/ml-dotnet-book/src/spam-detector/SpamDetector/EntryPoint.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let Main argv =
+    printfn "Hello World from F#!"
+
+    Console.ReadKey false |> ignore
+    0 
