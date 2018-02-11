93d79dbbfefa1490bb897debfc22c493db41503e
Jesse Squire <jesse.squire@gmail.com>
2018-02-11 16:56:00 -0500

    Basic console structure and sample tests for playtime with FsUnit

--- src/ml-dotnet-book/src/SpamDetector.UnitTests/Tests.fs
+++ src/ml-dotnet-book/src/SpamDetector.UnitTests/Tests.fs
@@ -1,8 +1,29 @@
-module MachineLearningBook.SpamDetector
+namespace MachineLearningBook.SpamDetector.UnitTests
 
-open System
-open Xunit
+    open System
+    open Xunit
+    open FsUnit.Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+
+    // http://fsprojects.github.io/FsUnit/index.html
+    // http://fsprojects.github.io/FsUnit/xUnit.html
+
+    module SampleTests =
+
+
+        [<Fact>]
+        let ``My test`` () =
+           true |> should be True
+
+
+        type ``Describe a scenario here`` () =
+            let value = true
+
+            [<Fact>] member x.
+                ``when I do something, something is in then right state`` () =
+                    true |> should equal value
+
+            [<Fact>]
+            member verify.``that this thing works because it's cool`` () =
+                "bob" |> should equal "bob"
+
--- src/ml-dotnet-book/src/spam-detector/SpamDetector/EntryPoint.fs
+++ src/ml-dotnet-book/src/spam-detector/SpamDetector/EntryPoint.fs
@@ -1,10 +1,48 @@
 ﻿// Learn more about F# at http://fsharp.org
+namespace MachineLearningBook.SpamDetector
+    
+    open System
+    open System.Diagnostics
+    open System.Reflection
+    open Humanizer
+    open System.IO
 
-open System
+    module EntryPoint =
 
-[<EntryPoint>]
-let Main argv =
-    printfn "Hello World from F#!"
 
-    Console.ReadKey false |> ignore
-    0 
+        [<EntryPoint>]
+        let Main argv =
+            let dataSetFile    = "sms-spam.txt"
+            let rootPath       = sprintf "%s/../../.././../../../../data" (Assembly.GetEntryAssembly().Location)
+            let trainingPath   = Path.GetFullPath(Path.Combine(rootPath, "training" + dataSetFile))
+            let validationPath = Path.GetFullPath(Path.Combine(rootPath, "validation" + dataSetFile))
+            let runWatch       = new Stopwatch()
+            let watch          = new Stopwatch()
+        
+            printfn ""
+            printfn "Run started using:"
+            printfn "\tTraining Data Path: %s" trainingPath
+            printfn "\tValidaiton Data Path: %s" validationPath 
+            printfn ""
+        
+            runWatch.Start()
+            watch.Start()
+            printf "Preparing data..."
+        
+            watch.Stop();
+            printfn " complete.  Elapsed: %s" (watch.Elapsed.Humanize())
+        
+            //watch.Reset()
+            //watch.Start()
+        
+            printfn "%d classifications were correct." 0
+
+            runWatch.Stop()
+            printfn ""
+            printfn "Run complete.  Total time: %s" (runWatch.Elapsed.Humanize())            
+            
+            printfn ""
+            printfn ""
+            printf "Press any key"
+            Console.ReadKey true |> ignore
+            0 
